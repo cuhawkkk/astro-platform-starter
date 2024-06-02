5e19a22a97ddb625bb32b9deec96e23a6dd0bd00
cuhawkbiz2 <168378937+cuhawkbiz2@users.noreply.github.com>
2024-06-02 09:01:51 +0100

    Update rewrite.js

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -2,7 +2,7 @@ const rewrite = async (request, context) => {
     const path = context.geo?.country?.code === 'AU' ? '/edge/australia' : '/edge/not-australia';
     return new URL(path, request.url);
 };
-
+// test
 export const config = {
     path: '/edge'
 };
